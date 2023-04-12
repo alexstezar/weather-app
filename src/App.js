3504157e51fd63c82e589448a0262de649fcad7f
Alex Stezar <alexstezar@Alexs-MacBook-Air.local>
2023-04-12 10:29:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
+import startBg from "./pictures/start_bg.jpg";
+import clearDay from "./pictures/clearDay_bg.jpg";
+import cloudsDay from "./pictures/cloudsDay_bg.jpg";
+import rainDay from "./pictures/rainDay_bg.jpg";
+import sunnyDay from "./pictures/sunnyDay_bg.jpg";
+import clearNight from "./pictures/clearNight_bg.jpg";
+import notclearNight from "./pictures/notclearNight_bg.jpg";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+///import UilReact from "@iconscout/react-unicons/icons/uil-react"
+import {
+    UilTemperature,
+    UilTear,
+    UilWind,
+} from "@iconscout/react-unicons"
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const [data, setData] = useState({});
+    const [location, setLocation] = useState('');
+    const [theme, setTheme] = useState(startBg);
+    const [forecastData, setForecastData] = useState([]);
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=afd21f85d53737015ccf714380d168fc`
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=afd21f85d53737015ccf714380d168fc`
+    const searchLocation = (event) => {
+        if (event.key === 'Enter') {
+            axios.get(url).then((response) => {
+                setData(response.data)
+                console.log(response.data)
+                setThemeFeature(response.data)
+            });
+            axios.get(forecastUrl).then((response) =>{
+                const forecastList = response.data.list.filter((weather) => {
+                    return new Date(weather.dt_txt).getHours() === 12;
+                })
+                setForecastData(forecastList);
+            })
+            setLocation('')
+        }
+    }
+
+    const getDateName = (dateString) => {
+        var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        var d = new Date(dateString);
+        return days[d.getDay()];
+    }
+
+    const setThemeFeature = (data) => {
+        const time = Math.floor(new Date().getTime() / 1000);
+        if(data.sys.sunrise <= time && data.sys.sunset >= time){
+            if(data.weather[0].main === "Clear") setTheme(clearDay);
+            else if(data.weather[0].main === "Clouds") setTheme(cloudsDay);
+            else if(data.weather[0].main === "Rain") setTheme(rainDay);
+            else if(data.weather[0].main === "Sunny") setTheme(sunnyDay);
+        }
+        else{
+            if(data.weather[0].main === "Clear") setTheme(clearNight);
+            else setTheme(notclearNight);
+        }
+
+    }
+
+    return (
+        <div className="app" style={{backgroundImage: `url(${theme})`}}>
+            <div className="container">
+                <div className="top">
+                    <div className="favorites">
+                    </div>
+                </div>
+
+
+                <div className="middle">
+                    <div className="search">
+                        <input value={location}
+                           onChange={event => setLocation(event.target.value)}
+                           onKeyPress={searchLocation}
+                           placeholder='Enter Location'
+                           type="text"
+                        />
+                    </div>
+
+                    <div className="location">
+                        <h1>{data.name}</h1>
+                    </div>
+
+                    <div className="details">
+                        <div className="description">
+                            {data.weather ? <p>{data.weather[0].main}</p> : null}
+                            {data.name != undefined &&
+                                <img src={"http://openweathermap.org/img/wn/01d@2x.png"}
+                                 alt=""
+                                 width="100"
+                                 height="100"
+                             />}
+
+                        </div>
+
+                        <div className="temperature">
+                            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+                        </div>
+
+                        {data.name != undefined &&
+
+                            <div className="more">
+                                <div className="feels_like">
+                                    <UilTemperature size={15}/>
+                                    <p1>Feels Like: </p1>
+                                    {data.main ? <p1 className='bold'>{data.main.feels_like.toFixed()} °C</p1> : null}
+                                </div>
+
+                                <div className="wind_speed">
+                                    <UilWind size={15}/>
+                                    <p1>Wind: </p1>
+                                    {data.wind ? <p1 className='bold'>{data.wind.speed.toFixed()} Km/h</p1> : null}
+                                </div>
+
+                                <div className="humidity">
+                                    <UilTear size={15}/>
+                                    <p1>Humidity: </p1>
+                                    {data.main ? <p1 className='bold'>{data.main.humidity}%</p1> : null}
+                                </div>
+                            </div>
+                        }
+                    </div>
+                </div>
+
+
+                <div className="bottom">
+                    {forecastData.length != 0 &&
+                        <div className="hourlyForecast">
+                            <h3>Daily Forecast</h3>
+                            <hr />
+                        </div>
+                    }
+                    {forecastData.length != 0 &&
+                        <div className="forecast">
+                            {
+                                forecastData.map((dayWeather) =>(
+                                    <div>
+                                        <p>{getDateName(dayWeather.dt_txt)}</p>
+                                        <p>{dayWeather.main.feels_like.toFixed()} °C</p>
+                                    </div>
+                            ))}
+                        </div>
+                    }
+                </div>
+            </div>
+        </div>
+    );
 }
+
 
 export default App;
